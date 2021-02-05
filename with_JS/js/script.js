67d141797ade95ca72c9c2c01be2e8f57784cbf2
M4DB0T <traf1718@gmail.com>
2021-02-06 02:03:08 +0400

    update

---
+++ with_JS/js/script.js
@@ -0,0 +1,12 @@
+var names= ["ilkin" , "ali" ,"akshin"];
+for (i in names){
+	console.log(i);
+}
+for (i in names){
+	if (names[i]=="a"){
+		console.log(names[i]);
+	}
+	else{
+		console.log("non");
+	}
+}
